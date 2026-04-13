2345b6677993dacdce342feb7aeaa6809617ebc2
Muskan Mishra <muskanm2405@gmail.com>
2026-04-13 10:43:21 +0530

    Login

---
+++ src/Components/NightLife/Nightlife.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nightlife = () => {
+  return (
+    <div>
+      <h2>Nightlife Section</h2>
+      <p>This is the nightlife page.</p>
+    </div>
+  );
+};
+
+export default Nightlife;
